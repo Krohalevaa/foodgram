b1aa89e64e2195ade3a0e648e1eeb85f2c2bce13
Анна Крохалева <akrohaleva67@gmail.com>
2025-02-11 13:19:18 +0300

    fix loc 2

--- no.bak/users/pagination.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class CustomPagination(PageNumberPagination):
-    """Кастомная пагинация с размером страницы 6 элементов."""
-    page_size = 6

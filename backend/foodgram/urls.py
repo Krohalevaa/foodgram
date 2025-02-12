d8dd82d5761a6247829b28ad7b6b7547be92a76b
Анна Крохалева <akrohaleva67@gmail.com>
2025-02-12 23:31:35 +0300

    fix

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -3,6 +3,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = 'api'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),

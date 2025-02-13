eca68a14549266d33f600792ce4abbdf3bbb31b5
Анна Крохалева <akrohaleva67@gmail.com>
2025-02-14 01:46:26 +0300

    3

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -13,22 +13,6 @@ from recipes.models import (Recipe, Ingredient, Tag, RecipeIngredient,
 User = get_user_model()
 
 
-# class UserLoginSerializer(serializers.Serializer):
-#     """Сериализатор для аутентификации пользователей."""
-
-#     email = serializers.EmailField()
-#     password = serializers.CharField()
-
-#     def validate(self, data):
-#         """Проверяет корректность учетных данных пользователя."""
-#         email = data['email']
-#         password = data['password']
-#         user = authenticate(email=email, password=password)
-#         if user is None:
-#             raise serializers.ValidationError("Invalid credentials")
-#         return user
-
-
 class Base64ImageField(serializers.ImageField):
     """Поле для кодирования изображения в base64."""
 
@@ -95,9 +79,9 @@ class RecipeIngredientSerializer(serializers.ModelSerializer):
     def get_ingredient(self, obj):
         """Возвращает информацию об ингредиенте."""
         return {
-            "id": obj.ingredient.id,
-            "name": obj.ingredient.name,
-            "unit": obj.ingredient.unit
+            'id': obj.ingredient.id,
+            'name': obj.ingredient.name,
+            'unit': obj.ingredient.unit
         }
 
 
@@ -107,7 +91,6 @@ class RecipeIngredientInputSerializer(serializers.ModelSerializer):
     id = serializers.PrimaryKeyRelatedField(
         queryset=Ingredient.objects.all(),
         source='ingredient')
-    # amount = serializers.FloatField()
 
     class Meta:
         """Метаданные для настройки сериализатора ввода."""
@@ -120,7 +103,6 @@ class RecipeIngredientOutputSerializer(serializers.ModelSerializer):
     """Сериализатор для вывода ингредиентов в рецепте."""
 
     ingredient = IngredientSerializer(read_only=True)
-    # amount = serializers.FloatField()
 
     class Meta:
         """Метаданные для настройки сериализатора вывода."""
@@ -156,17 +138,15 @@ class RecipeSerializer(serializers.ModelSerializer):
             'tags', 'ingredients', 'image', 'slug', 'creation_date',
             'is_favorited', 'is_in_shopping_cart'
         ]
-        extra_kwargs = {"author": {"read_only": True}}
+        extra_kwargs = {'author': {'read_only': True}}
 
     def to_representation(self, instance):
         """Преобразует модель рецепта в словарь для сериализации."""
         representation = super().to_representation(instance)
-        # Сериализация тегов
         representation['tags'] = TagSerializer(
             instance.tags.all(),
             many=True
         ).data
-        # Формирование списка ингредиентов
         representation['ingredients'] = [
             {
                 'id': recipe_ingredient.ingredient.id,
@@ -178,29 +158,14 @@ class RecipeSerializer(serializers.ModelSerializer):
         ]
         return representation
 
-    # def to_representation(self, instance):
-    #     """Преобразует модель рецепта в словарь для сериализации."""
-    #     rep = super().to_representation(instance)
-    #     rep['tags'] = TagSerializer(instance.tags.all(), many=True).data
-    #     rep['ingredients'] = [
-    #         {
-    #             'id': ri.ingredient.id,
-    #             'name': ri.ingredient.name,
-    #             'unit': ri.ingredient.unit,
-    #             'amount': ri.amount
-    #         }
-    #         for ri in instance.recipe_ingredients.all()
-    #     ]
-    #     return rep
-
     def get_author(self, obj):
         """Получает информацию об авторе рецепта."""
         author = obj.author
         return {
-            "id": author.id,
-            "username": author.username,
-            "first_name": author.first_name,
-            "last_name": author.last_name,
+            'id': author.id,
+            'username': author.username,
+            'first_name': author.first_name,
+            'last_name': author.last_name,
             'avatar': obj.author.avatar.url if obj.author.avatar else None
         }
 
@@ -241,36 +206,6 @@ class RecipeSerializer(serializers.ModelSerializer):
         instance = super().update(instance, validated_data)
         self._update_tags_and_ingredients(instance, tags, ingredients_data)
         return instance
-    # def create(self, validated_data):
-    #     """Создает новый рецепт на основе валидных данных."""
-    #     ingredients_data = validated_data.pop('recipe_ingredients', [])
-    #     tags = validated_data.pop('tags', [])
-    #     recipe = Recipe.objects.create(**validated_data)
-    #     recipe.tags.set(tags)
-    #     ingredients = [
-    #         RecipeIngredient(
-    #             recipe=recipe,
-    #             ingredient=item['ingredient'],
-    #             amount=item['amount']
-    #         ) for item in ingredients_data]
-    #     RecipeIngredient.objects.bulk_create(ingredients)
-    #     return recipe
-
-    # def update(self, instance, validated_data):
-    #     """Обновляет существующий рецепт на основе валидных данных."""
-    #     ingredients_data = validated_data.pop('recipe_ingredients', [])
-    #     tags = validated_data.pop('tags', [])
-    #     instance = super().update(instance, validated_data)
-    #     instance.tags.set(tags)
-    #     instance.recipe_ingredients.all().delete()
-    #     ingredients = [
-    #         RecipeIngredient(
-    #             recipe=instance,
-    #             ingredient=item['ingredient'],
-    #             amount=item['amount']
-    #         ) for item in ingredients_data]
-    #     RecipeIngredient.objects.bulk_create(ingredients)
-    #     return instance
 
     def get_is_in_shopping_cart(self, obj):
         """Метод для определения, находится ли рецепт в списке покупок."""
@@ -290,7 +225,6 @@ class FavoriteRecipeSerializer(serializers.ModelSerializer):
 
         model = FavoriteRecipe
         fields = ('id', 'user', 'recipe')
-        # fields = '__all__'
 
 
 class ShoppingListSerializer(serializers.ModelSerializer):
@@ -301,7 +235,6 @@ class ShoppingListSerializer(serializers.ModelSerializer):
 
         model = ShoppingList
         fields = ('id', 'user', 'recipe')
-        # fields = '__all__'
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -330,13 +263,6 @@ class UserSerializer(serializers.ModelSerializer):
                 subscriber=user).exists()
         return False
 
-    # def update(self, instance, validated_data):
-    #     """Обновляет данные экземпляра модели на основе данных."""
-    #     for attr, value in validated_data.items():
-    #         setattr(instance, attr, value)
-    #     instance.save()
-    #     return instance
-
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     """Сериализатор для подписок пользователей."""
@@ -358,7 +284,6 @@ class SubscriptionSerializer(serializers.ModelSerializer):
         model = Subscription
         fields = ('id', 'author', 'subscriber', 'recipes',
                   'username', 'first_name', 'last_name', 'avatar')
-        # fields = '__all__'
 
     def get_recipes(self, obj):
         """Возвращает список рецептов автора подписки."""
@@ -374,15 +299,3 @@ class SubscriptionSerializer(serializers.ModelSerializer):
                                       many=True,
                                       context=self.context)
         return serializer.data
-        # recipes_limit = self.context.get('recipes_limit')
-        # recipes_qs = obj.author.recipes.all()
-        # if recipes_limit:
-        #     try:
-        #         recipes_limit = int(recipes_limit)
-        #         recipes_qs = recipes_qs[:recipes_limit]
-        #     except ValueError:
-        #         pass
-        # serializer = RecipeSerializer(recipes_qs,
-        #                               many=True,
-        #                               context=self.context)
-        # return serializer.data
--- backend/api/views.py
+++ backend/api/views.py
@@ -177,7 +177,9 @@ class UserViewSet(viewsets.ModelViewSet):
 class RecipeViewSet(viewsets.ModelViewSet):
     """Вьюсет для работы с рецептами."""
 
-    queryset = Recipe.objects.all()
+    # queryset = Recipe.objects.all()
+    queryset = Recipe.objects.all().select_related('author').prefetch_related(
+        'tags', 'ingredients')
     serializer_class = RecipeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     pagination_class = CustomPagination
@@ -284,6 +286,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def download_shopping_cart(self, request, *args, **kwargs):
         """Скачивание списка покупок для текущего пользователя."""
         shopping_list = ShoppingList.objects.filter(user=request.user)
+        ingredients = self.get_ingredients_from_shopping_list(shopping_list)
+        content = self.generate_shopping_cart_content(ingredients)
+
+        response = HttpResponse(content, content_type='text/plain')
+        response[
+            'Content-Disposition'] = 'attachment; filename="shopping_list.txt"'
+        return response
+
+    def get_ingredients_from_shopping_list(self, shopping_list):
+        """Извлекает ингредиенты из корзины покупок пользователя."""
         ingredients = defaultdict(float)
         for item in shopping_list:
             for recipe_ingredient in item.recipe.recipe_ingredients.all():
@@ -291,12 +303,30 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 ingredients[
                     f"{ingredient.name} ({ingredient.unit})"
                 ] += recipe_ingredient.amount
-        content = "\n".join(
+        return ingredients
+
+    def generate_shopping_cart_content(self, ingredients):
+        """Генерирует контент для скачивания в виде текста."""
+        return '\n'.join(
             f"{name} — {amount}" for name, amount in ingredients.items())
-        response = HttpResponse(content, content_type='text/plain')
-        response[
-            'Content-Disposition'] = 'attachment; filename="shopping_list.txt"'
-        return response
+    # @action(detail=False, methods=['get'], url_path='download_shopping_cart')
+    # def download_shopping_cart(self, request, *args, **kwargs):
+    #     """Скачивание списка покупок для текущего пользователя."""
+    #     shopping_list = ShoppingList.objects.filter(user=request.user)
+    #     ingredients = defaultdict(float)
+    #     for item in shopping_list:
+    #         for recipe_ingredient in item.recipe.recipe_ingredients.all():
+    #             ingredient = recipe_ingredient.ingredient
+    #             ingredients[
+    #                 f"{ingredient.name} ({ingredient.unit})"
+    #             ] += recipe_ingredient.amount
+    #     content = "\n".join(
+    #         f"{name} — {amount}" for name, amount in ingredients.items())
+    #     response = HttpResponse(content, content_type='text/plain')
+    #     response[
+    #         'Content-Disposition'] = 'attachment;
+    # filename="shopping_list.txt"'
+    #     return response
 
 
 class FavoriteViewSet(viewsets.ModelViewSet):

cb36f2d6b082a536e1b19ea158302b87e791e165
Анна Крохалева <akrohaleva67@gmail.com>
2025-02-17 18:06:22 +0300

    urls2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path, include
 
 from django.contrib import admin
-from django.conf import settings
+# from django.conf import settings
 from rest_framework.routers import DefaultRouter
 
 from .views import (RecipeViewSet, TagViewSet, IngredientViewSet,

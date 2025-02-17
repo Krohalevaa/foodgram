ba505a4f7c9b4beb9f0fe9bfcc7b987ae876d8bc
Анна Крохалева <akrohaleva67@gmail.com>
2025-02-17 18:03:43 +0300

    urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
          name='user-avatar-update'),
 ]
 
-if settings.DEBUG:
-    from django.conf import settings
-    from django.conf.urls.static import static
+# if settings.DEBUG:
+#     from django.conf import settings
+#     from django.conf.urls.static import static
 
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
